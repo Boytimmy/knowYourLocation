e89c42a083fc177cbc8f5bcf8dbef80679da04c5
Timmy <olalekantimileyin2005@gmail.com>
2023-09-13 11:25:57 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,10 @@
+const dispLocation = () =>{
+  navigator.geolocation.getCurrentPosition((posi)=>{
+    console.log(posi.coords);
+    let lati =(posi.coords.latitude).toFixed(2)
+    let longi =(posi.coords.longitude).toFixed(2)
+    showLocation.innerHTML = `
+    <h3 align="center">You are currently at ${lati} &deg;N and ${longi} &deg;E</h3>
+    `
+  })
+}
